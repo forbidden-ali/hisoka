fd38df52003c8b599d55be09e26c686eff574a4a
quininer <quininer@live.com>
2014-10-17 11:42:05 +0800

    .. fix install readline

--- install.js
+++ install.js
@@ -1,19 +1,15 @@
 var mongoose = require('mongoose'),
-    fs = require('fs');
+    fs = require('fs'),
+    readline = require('readline'),
     models = require('./config/models');
 
-function read(prompt, callback){
-    process.stdout.write(prompt+':');
-    process.stdin.resume();
-    process.stdin.setEncoding('utf-8');
-    process.stdin.on('data', function(chunk){
-        process.stdin.pause();
-        callback(chunk);
-    });
-};
+var rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
 
-function createdb(db, name, passwd, salt, callback){
-    mongoose.connect(config.db);
+function adduser(db, name, passwd, salt, callback){
+    mongoose.connect(db);
 
     var passwd = sql.hash(passwd+salt);
     var user = new models.User({
@@ -41,31 +37,32 @@ function main(){
     var config = {},
         user = {};
     console.log('It will help you install Hisoka.');
-    console.log('               quininer@live.com\n')
+    console.log('                                              quininer@live.com\n')
 
     console.log('First, the configuration database,','This should be a Mongodb URL.');
     console.log('*Including configuring the account password, and database name');
-    read('db[openshift]', function(input){
-        config.db = input||'mongodb://'+
+    rl.question('db[openshift]:', function(input){
+        config.db = input||('mongodb://'+
         process.env.OPENSHIFT_MONGODB_DB_USERNAME+':'+
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD+'@'+
         process.env.OPENSHIFT_MONGODB_DB_HOST+':'+
-        process.env.OPENSHIFT_MONGODB_DB_PORT+'/hisoka';
+        process.env.OPENSHIFT_MONGODB_DB_PORT+'/hisoka');
         console.log('Here configuration Hisoka account.');
-        read('name', function(input){
+        rl.question('name:', function(input){
             user.name = input;
-            read('passwd', function(input){
+            rl.question('passwd:', function(input){
                 user.passwd = input;
-                console.log('Creating a db...')
-                createdb(config.db, user.name, user.passwd, Math.random().toString(36).slice(2), function(){
+                console.log()
+                console.log('Creating a db...');
+                adduser(config.db, user.name, user.passwd, Math.random().toString(36).slice(2), function(){
                     console.log("Here's server configuration Hisoka.");
-                    read('ip[openshift]', function(input){
+                    rl.question('ip[openshift]:', function(input){
                         config.ip = input||process.env.OPENSHIFT_NODEJS_IP;
-                        read('port[openshift]', function(input){
+                        rl.question('port[openshift]:', function(input){
                             config.ip = input||process.env.OPENSHIFT_NODEJS_PORT||8080;
-                            read('ssl[null]', function(input){
+                            rl.question('ssl[null]:', function(input){
                                 config.ssl = input||null;
-                                read("host['']", function(input){
+                                rl.question("host['']:", function(input){
                                     config.host = input||'';
                                     createjson(config, function(err){
                                         if(err){
